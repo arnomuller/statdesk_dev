f453df30d63f2724f0e3f4fa59fefd0b70a103f4
arnomuller <84503348+arnomuller@users.noreply.github.com>
2023-04-27 11:52:24 +0200

    Petit changement
    - test d'installation de dépendances automatiques

--- package 'statdesk'/Installer statdesk.R
+++ package 'statdesk'/Installer statdesk.R
@@ -1,4 +1,30 @@
-install.packages("D:/chemin/statdesk_0.1.0.tar.gz", repos = NULL, type = "source")
+
+
+# Installation en local ----
+
+# Liste des dépendances
+.packages = c("shiny", "shinyWidgets", "shinythemes", "fresh", "openxlsx", "dplyr", "sortable",
+              "tidyr", "DT", "ggplot2", "colourpicker", "RColorBrewer", "plotly")
+# Installation des manquantes
+.new.packages <- .packages[!(.packages %in% installed.packages()[,"Package"])]
+if(length(.new.packages)) install.packages(.new.packages)
+
+# Installer statdesk
+install.packages("./statdesk_0.1.0.tar.gz", repos = NULL, type = "source")
+# Lancer statdesk
 library(statdesk)
 statdesk()
+
+
+# Installation depuis Github ----
+
+install.packages("devtools")
+library("devtools")
+install_github("arnomuller/statdesk")
+
+
+
+
+
+
 
